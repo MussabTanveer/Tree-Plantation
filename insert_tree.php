f49523ceb6450226de45cc1e8978476957d31ac5
MussabTanveer <mussab.tanveer9@gmail.com>
2018-06-05 01:28:57 +0500

    insert type of tree to db

--- insert_tree.php
+++ insert_tree.php
@@ -5,18 +5,20 @@ require("connection.php");
 $info = $_POST["info"];
 $xcoord = $info[0];
 $ycoord = $info[1];
-//$username = "John";
-//$email = "john@example.com";
 $username = $info[2];
 $email = $info[3];
+$type = $info[4];
 
-$sql = "INSERT INTO tree_location (xcoord, ycoord, username, email) VALUES($xcoord, $ycoord, '$username', '$email')";
-
-if(mysqli_query($conn, $sql)){
-	echo "<font color = green> Tree planted sucessfully </font>";
+$sql = "INSERT INTO tree_location (type, xcoord, ycoord, username, email) VALUES('$type', $xcoord, $ycoord, '$username', '$email')";
+if(filter_var($email,FILTER_VALIDATE_EMAIL))
+{
+	if(mysqli_query($conn, $sql)){
+		echo "<font color='green'> Congratulations! You have planted a tree. :) </font>";
+	}
+	else
+		echo "<font color='red'> Tree not planted </font>";
 }
-else{
-	echo "<font color = red> Tree not planted </font>";
-}
+else
+	echo "<font color='red'> Please Enter Valid Email address </font>";
 
 ?>
