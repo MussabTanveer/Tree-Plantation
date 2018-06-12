170e45763939504e2b2dbf1d8928e3f8bfdd6579
MussabTanveer <mussab.tanveer9@gmail.com>
2018-06-13 00:16:46 +0500

    add stylesheets, scripts and dynamic links

--- header.php
+++ header.php
@@ -10,11 +10,14 @@
     <title>Tree Plantation</title>
     <script src="./script/jquery/jquery-3.2.1.min.js"></script>
     <script src="./bootstrap-3.3.7/js/bootstrap.min.js"></script>
+    <script src="./script/custom-javascript.js"></script>
     <link rel="shortcut icon" href="./images/pine-tree.png" />
     <link rel="stylesheet" href="./bootstrap-3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="./font-awesome-4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="./css/Footer-with-button-logo.css">
+    <link rel="stylesheet" href="./css/Login.css">
+    <link rel="stylesheet" href="./css/BackButton.css">
 </head>
 <body>
     <nav class="navbar navbar-inverse">
@@ -34,8 +37,20 @@
             <li><a href="./forbidden_area.php">Forbidden Area</a></li>
         </ul>
         <ul class="nav navbar-nav navbar-right">
-            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            <li><a href="./signup.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+            <?php
+            if(isset($_SESSION['login']) && $_SESSION['login'] == true){
+            ?>
+            <li><a href="./login.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+            <li><a onclick="deleteAccount('<?php echo $_SESSION['username']; ?>')"><span class="glyphicon glyphicon-trash"></span> Delete account</a></li>
+            <?php
+            }
+            else {
+            ?>
+            <li><a href="./login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            <?php
+            }
+            ?>
         </ul>
         </div>
     </div>
